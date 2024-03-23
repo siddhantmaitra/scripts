ec946c23c9d24eedcb5889f661c27fdaa8b4ef66
Siddhant Maitra <smaitra.dev@gmail.com>
2024-03-24 00:00:35 +0530

    update setup script

--- working/setup.sh
+++ working/setup.sh
@@ -1,52 +1,71 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -eu
+
+# ANSI color codes
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m' # No color
+
+# Function to print colored messages
+print_info() {
+    echo -e "${GREEN}[INFO] $1${NC}"
+}
+
+print_warning() {
+    echo -e "${YELLOW}[WARN] $1${NC}"
+}
+
+print_error() {
+    echo -e "${RED}[ERROR] $1${NC}"
+}
 
 check_and_install() {
     for tool in "$@"; do
-        if command -v $tool >/dev/null 2>&1; then
-            echo "[INFO] $tool is already installed."
+        if command -v "$tool" >/dev/null 2>&1; then
+            print_info "$tool is already installed."
         else
-            echo "[WARN] $tool is not installed. Would you like to install it? (y/n)"
-            read answer
-            if [ "$answer" != "${answer#[Yy]}" ]; then
-                sudo pacman -Sy $tool --noconfirm
-                if command -v $tool >/dev/null 2>&1; then
-                    echo "[INFO] $tool has been installed successfully."
-                else
-                    echo "[ERROR] There was a problem installing $tool. Please try again."
-                fi
+            print_warning "$tool is not installed, installing it..."
+            sudo pacman -Sy "$tool" --noconfirm
+            if command -v "$tool" >/dev/null 2>&1; then
+                print_info "$tool has been installed successfully."
+            else
+                print_error "There was a problem installing $tool. Please try again."
             fi
         fi
     done
 }
 
+figlet -ct " Archbox Setup Script" 
 echo "Please enter your GitHub username:"
-read USERNAME
+read -r USERNAME
 
-check_and_install ansible chezmoi
+check_and_install ansible chezmoi figlet cowsay
 
 # Run chezmoi init
-echo "[INFO] Running chezmoi init..."
-chezmoi init --apply $USERNAME
+print_info "Running chezmoi init..."
+chezmoi init --apply "$USERNAME"
 
 # Clone the git repository
-echo "[INFO] Cloning 'device-setup' repository..."
+print_info "Cloning 'device-setup' repository..."
 if [ -d ~/.config/device-setup ]; then
-	rm -rf ~/.config/device-setup
+    rm -rf ~/.config/device-setup
 fi
 
-git clone https://github.com/$USERNAME/device-setup.git ~/.config/device-setup
+git clone "https://github.com/$USERNAME/device-setup.git" ~/.config/device-setup
 
 cd ~/.config/device-setup
-echo "[INFO] Running ansible playbook..."
+# Run ansible playbook
+print_info "Running ansible playbook..."
 ansible-playbook -v playbook.yml --ask-become-pass
 
 # Change the remote URL to SSH based URL
-echo "[INFO] Changing the remote URL to SSH based URL..."
-git remote set-url origin git@github.com:$USERNAME/device-setup.git
-# Dir is home now!
-cd ~
-# Finishing messages
-echo "[INFO] Setup is complete. Don't forget to setup SSH!"
+print_info "Changing the remote URL to SSH based URL..."
+git remote set-url origin "git@github.com:$USERNAME/device-setup.git"
 
+# Return to home directory
+cd ~
+
+# Finishing messages
+print_info "Setup is complete. Don't forget to set up SSH!"
 
